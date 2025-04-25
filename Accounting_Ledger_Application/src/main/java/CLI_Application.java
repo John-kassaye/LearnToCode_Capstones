43256dc86410419304b4a79825c482728d011092
Yohannes Kassaye <jmule1567@gmail.com>
2025-04-25 15:42:39 -0700

    creating a home screen method
    I used a do-while loop to run the app until the user enters X.
    
    The reason I used do-while is because it runs at least once before checking the condition.

--- Accounting_Ledger_Application/src/main/java/CLI_Application.java
+++ Accounting_Ledger_Application/src/main/java/CLI_Application.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class CLI_Application {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        homeScreen();
+    }
+
+    public static void homeScreen(){
+
+        String choice = "";
+        do {
+            System.out.println("""
+                    Welcome to CLI Application
+             D) Add Deposit
+             P) Make Payment(Debit)
+             L) Ledger
+             X) Exit   \s""");
+             choice = scanner.nextLine();
+        } while (!choice.trim().equalsIgnoreCase("x"));
+    }
 }
